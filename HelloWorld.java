a2104accf0a180a05e0a5be9de2987ae6d188d79
DonDeWitt <dewitt.djd3@gmail.com>
2021-06-27 10:41:27 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author Donald_DeWitt
+ * This program is the classic hello world program,
+ * made to explore the functionality of Java.
+ */
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+
+	} //End main method
+
+} //End class
